5e80e6e3d1aaca0ca492a8943f8d586d7d764f2f
mauropereira <mauro.a.pereira@gmail.com>
2022-11-29 01:11:16 -0300

    feat(scrip.js): se agrega un anidado en console.log

--- js_course/js/script.js
+++ js_course/js/script.js
@@ -1,10 +1,11 @@
 let nombre = prompt("Ingresa tu nombre");
 let apellido = prompt("Ingresa tu apellido");
 
-if (nombre == "" || apellido == "") {
-  console.log("Algún campo está incompleto")
+if (nombre == ("" || null) || apellido == ("" || null)) {
+  console.log("Algún campo o todos están incompleto")
 }
 else {
   console.log("Bienvenido/a " + nombre + " " + apellido)
 
 }
+console.log(12 == (3 * 4))
