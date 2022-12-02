05df5bbd8003639c32c02398ee62be94f8773eac
MauroPereira <mauro.a.pereira@gmail.com>
2022-12-02 18:17:54 -0300

    Se copia script a script_temp

---
+++ PreEntrega1Pereira/js/script_temp.js
@@ -0,0 +1,30 @@
+// Variables
+
+// Funciones
+
+// Declaraciones de funciones
+function saludarConNombreApellido(nombre = "desconocido", apellido = "desconocido") {
+  console.log("Buen dia " + nombre + ", de apellido " + apellido);
+}
+
+function sumar(num1, num2) {
+  console.log(num1 + num2)
+}
+
+const suma = (a, b) => a + b;
+
+//let nombre = prompt("Ingrese un nombre"); // si no se ingresa nada toma un valor ""
+//let apellido = prompt("Ingrese un nombre");
+
+let numero1 = parseInt(prompt("Ingrese el primer número"));
+let numero2 = parseInt(prompt("Ingrese el segundo número"));
+
+
+// Invocaciones
+//saludarConNombreApellido();
+//saludarConNombreApellido(nombre, apellido);
+sumar(numero1, numero2);
+console.log(suma(numero1, numero2));
+
+
+
