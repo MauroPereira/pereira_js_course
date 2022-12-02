6f7debad825798a6809a02aef57f5c19a4bd2a78
mauropereira <mauro.a.pereira@gmail.com>
2022-12-01 21:09:30 -0300

    eso no funciona, no importa

--- js_course/js/script.js
+++ js_course/js/script.js
@@ -6,10 +6,24 @@
 function saludarConNombreApellido(nombre = "desconocido", apellido = "desconocido") {
   console.log("Buen dia " + nombre + ", de apellido " + apellido);
 }
+const sumar = function (num1, num2) {
+  console.log(num1 + num2)
+}
 
-let nombre = prompt("Ingrese un nombre"); // si no se ingresa nada toma un valor ""
-let apellido = prompt("Ingrese un nombre");
+//let nombre = prompt("Ingrese un nombre"); // si no se ingresa nada toma un valor ""
+//let apellido = prompt("Ingrese un nombre");
+
+let numero1 = parseInt(prompt("Ingrese el primer número"));
+let numero2 = parseInt(prompt("Ingrese el segundo número"));
+
 
 // Invocaciones
-saludarConNombreApellido();
-saludarConNombreApellido(nombre, apellido);
+//saludarConNombreApellido();
+//saludarConNombreApellido(nombre, apellido);
+sumar(numero1, numero2);
+otro_nombre = sumar;
+otro_nombre(numero1, numero2)
+
+
+
+
